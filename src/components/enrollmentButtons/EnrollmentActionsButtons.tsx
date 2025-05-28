bcedccb0a947529ca748a0291eea546e230f2e2d
cnandza <cnandza@saudigitus.org>
2025-05-28 14:41:50 +0200

    fix navigation to transfer-execute

--- src/components/enrollmentButtons/EnrollmentActionsButtons.tsx
+++ src/components/enrollmentButtons/EnrollmentActionsButtons.tsx
@@ -3,24 +3,23 @@ import Tooltip from '@material-ui/core/Tooltip';
 import TabComponent from '../tabs/TabComponent';
 import { IconAddCircle24, Button } from "@dhis2/ui";
 import { useUrlParams } from 'dhis2-semis-functions';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import styles from './enrollmentActionsButtons.module.css'
 
 function EnrollmentActionsButtons({ selectedValue, setSelectedValue }: { setSelectedValue: (args: any) => void, selectedValue: string }) {
     const { search } = useLocation()
+    const navigate = useNavigate()
     const { urlParameters } = useUrlParams();
     const { school: orgUnit } = urlParameters();
 
     return (
         <div className={styles.container}>
             <TabComponent selectedValue={selectedValue} setSelectedValue={setSelectedValue} />
-            <Link to={`semis/transfer-execute${search}`}>
-                <Tooltip title={orgUnit === null ? "Please select an organisation unit before" : ""} >
-                    <Button className={styles.btn} disabled={!Boolean(orgUnit)} icon={<IconAddCircle24 />}>
-                        <span className={styles.work_buttons_text}>Perform transfer</span>
-                    </Button>
-                </Tooltip>
-            </Link>
+            <Tooltip title={orgUnit === null ? "Please select an organisation unit before" : ""} >
+                <Button onClick={() => navigate(`/semis/transfer-execute${search}`)} className={styles.btn} disabled={!Boolean(orgUnit)} icon={<IconAddCircle24 />}>
+                    <span className={styles.work_buttons_text}>Perform transfer</span>
+                </Button>
+            </Tooltip>
         </div>
     )
 }

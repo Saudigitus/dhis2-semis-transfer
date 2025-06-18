f29b6337cb49dc788d5d5fcc51fbd4148cac893b
cnandza <cnandza@saudigitus.org>
2025-06-18 11:36:40 +0200

    chore: update version to 1.0.0-alpha.12 and simplify Transfer component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,11 @@
 import './app.css'
 import React from 'react'
 import { Router } from '../components/routes'
-import { AppWrapper } from 'dhis2-semis-components'
-import { HashRouter } from 'react-router-dom'
-import { useConfig } from '@dhis2/app-runtime'
 
 const Transfer = () => {
-    const { baseUrl } = useConfig()
 
     return (
-        <AppWrapper baseUrl={baseUrl} dataStoreKey='dataStore/semis/values'>
-            <HashRouter>
-                <Router />
-            </HashRouter>
-        </AppWrapper>
+        <Router />
     )
 }
 
